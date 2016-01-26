2b30916f7709a44d005cedc0878c8769ebf8f7e9
Robbert-Jan de Jager <robojan1@hotmail.com>
2016-01-26 20:33:07 +0100

    Added test font

---
+++ emgl/src/arial12.h
@@ -0,0 +1,45 @@
+#ifndef _FONT_ARIAL12_H_
+#define _FONT_ARIAL12_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+// Choose between local declaration or remote declaration 
+//#include "font_data.h"
+
+typedef struct {
+	int16_t advanceX;
+	int16_t advanceY;
+	int16_t bitmapLeft;
+	int16_t bitmapTop;
+	uint16_t bitmapWidth;
+	uint16_t bitmapHeight;
+	uint32_t bitmapSize;
+	const uint8_t * const bitmapData;
+} EMGL_glyph_t;
+
+typedef struct {
+	uint32_t startCode;
+	uint32_t endCode;
+	char name[17];
+	const EMGL_glyph_t * const * const glyphPtrs;
+} EMGL_codePage_t;
+
+typedef struct {
+	int16_t ascender;
+	uint8_t bpp;
+	uint8_t numCodepages;
+	const EMGL_codePage_t * const codePagePtrs;
+} EMGL_font_t;
+
+
+extern const EMGL_font_t font_arial12;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
